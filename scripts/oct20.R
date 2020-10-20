648b1a3e50b5aa54982f3157edfb886561dae0d7
Mark Andrews <mjandrews.org@gmail.com>
2020-10-20 15:40:36 +0100

    Update Oct20.R script

--- scripts/oct20.R
+++ scripts/oct20.R
@@ -14,12 +14,48 @@ ggplot(ansur_df,
 
 # Make a scatterplot of weight by height
 # with colour coding of gender
-ggplot(ansur_df, 
-       aes(x = height, y = weight, colour = gender)
-) + geom_point(size = 0.5)
+ggplot(mapping = aes(x = height, y = weight, colour = gender),
+       data = ansur_df) + geom_point(size = 0.5)
 
 
 # Make a histogram of weight by gender
 ggplot(ansur_df, 
        aes(x = weight, fill = gender)
 ) + geom_histogram(binwidth = 5, colour = 'white')
+
+
+# an example custom function
+f <- function(x, y) {x/y}
+
+
+# Make a histogram of weight by height tercile
+ggplot(ansur_df, 
+       aes(x = weight)
+) + geom_histogram(binwidth = 5, colour = 'white') +
+  facet_wrap(~height_tercile)
+
+# calculate mean and sd of weight 
+summarize(ansur_df, 
+          average = mean(weight),
+          st_dev = sd(weight))
+
+
+# calculate mean and sd of weight 
+# for each height tercile group
+summarize(group_by(ansur_df, height_tercile), 
+          average = mean(weight),
+          st_dev = sd(weight))
+
+# calculate mean and sd of weight 
+# for each height tercile group (using the %>% )
+group_by(ansur_df, height_tercile) %>% 
+  summarize(average = mean(weight),
+            st_dev = sd(weight))
+
+
+# calculate mean and sd of weight, and mean of height,
+# for each height tercile group
+summarize(group_by(ansur_df, height_tercile), 
+          average = mean(weight),
+          st_dev = sd(weight),
+          avg_height = mean(height))
